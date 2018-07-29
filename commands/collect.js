9379d51ffa9070dc10fa76dabb1be12d1a88cccd
Joe Koshakow <koshy44@gmail.com>
2018-07-29 12:09:26 -0400

    added collect command

---
+++ commands/collect.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'collect-poop',
+    description: 'collect\'s your poop',
+    usage: ['', '<max poops>'],
+    aliases: ['poop'],
+	execute(message, args) {
+        const filter = m => m.content.includes('poop') && m.author.id == message.author.id;
+    
+        if(args) {
+            const collector = message.channel.createMessageCollector(filter, {time: 15000, max: args[0]});
+        } else {
+            const collector = message.channel.createMessageCollector(filter, {time: 15000});
+        }
+
+        collector.on('collect', m => {
+            m.react('💩')
+                .catch((e) => m.reply('Looks like I\'m constipated: '+e));
+        });
+
+        collector.on('end', collected => {
+            message.channel.send(`I pooped ${collected.size} times!`);
+        });
+    },
+};
