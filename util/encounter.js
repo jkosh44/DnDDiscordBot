4813fad6dc40356962b61cf7154b6a3d72161b9a
Joe Koshakow <koshy44@gmail.com>
2018-08-04 11:11:57 -0400

    fixed random encounter comparison

--- util/encounter.js
+++ util/encounter.js
@@ -1,8 +1,9 @@
 function encounter(weightedDist, totalWeight) {
     const randomNum = Math.random()*totalWeight;
+    console.log(`random num = ${randomNum}`)
     for(var i=0; i<weightedDist.length; i++) {
         const weightedRange = weightedDist[i].weightedRange;
-        if(weightedRange >= randomNum) {
+        if(weightedRange > randomNum) {
             const content = weightedDist[i].content;
             return content;
         }
