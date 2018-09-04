3777f52c619264f43f8f92e5c2b715e97069238d
Joe Koshakow <koshy44@gmail.com>
2018-09-03 22:08:13 -0400

    removed uneccessary method

--- db/ability-db.js
+++ db/ability-db.js
@@ -1,19 +1,5 @@
 const {Ability} = require('./Schema.js');
 
-async function addAbility(description) {
-    try {
-        const ability = await Ability.create({
-            ability_description: description
-        });
-        console.log(`created new ability" ${ability.ability_description}`);
-        return ability;
-    }
-    catch(e) {
-        console.log(e);
-        return null;
-    }
-}
-
 async function getAbilityById(ability_id) {
     return await Ability.findById(ability_id);
 }
@@ -24,7 +10,6 @@ async function getAbilityByDesc(ability_description) {
 
 module.exports = {
     AbilityDb: {
-        addAbility,
         getAbilityById,
         getAbilityByDesc
     }
